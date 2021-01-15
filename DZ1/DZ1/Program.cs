1ae470e7b8943c6640fa41ea8c377557594d64eb
DenisZyryanov <dziro744@gmail.com>
2021-01-15 23:42:13 +0700

    Initial commit

---
+++ DZ1/DZ1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DZ1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Как вас зовут? ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Привет, "+name+", сегодня: "+DateTime.Now);
+        }
+    }
+}
